d304dcbd9fb10d87030d4b6c8cfde18cb3aeb6df
Daniel <piccoli@gmail.com>
2026-04-17 19:57:52 -0300

    Improved data manager load function but still far from definitive

--- data/data_manager.py
+++ data/data_manager.py
@@ -58,6 +58,18 @@ class DataManager:
             df['ticker'] = ticker
             e = Index(ticker=ticker, calendar=CalendarType.US, currency='USD', market_data=df, metadata=meta_data)
             r[ticker] = e
+        elif ticker[-6:] == 'Equity':
+            lib_data = lib.read(ticker)
+            df = lib_data.data
+            meta_data = lib_data.metadata
+            df['ticker'] = ticker
+            if ticker[-9:-6] == 'US':
+                f = Equity(ticker=ticker, calendar=CalendarType.US, maturity=dt.datetime.max, cost_bps=0.0,
+                       cost_unit=0.005, market_data=df, days2settle=1, metadata=meta_data)
+            else:
+                f = Equity(ticker=ticker, calendar=CalendarType.B3, maturity=dt.datetime.max, cost_bps=0.0,
+                           cost_unit=0.005, market_data=df, days2settle=1, metadata=meta_data)
+            r[ticker] = f
         else:
             lib_data = lib.read(ticker)
             df = lib_data.data
