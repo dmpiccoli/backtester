0415a75c1883a0724673d24f781d385b1d42518b
Daniel <piccoli@gmail.com>
2026-04-28 19:07:43 -0300

    Risk parity model on ETFs

--- model/portfolio_manager/risk_parity.py
+++ model/portfolio_manager/risk_parity.py
@@ -1,12 +1,47 @@
 import datetime as dt
 
 import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+from sklearn.covariance import LedoitWolf
 
 from data.data_manager import DataManager
 from model.future import FutureCode
 from utils.pcalendar import CalendarType
 from model.portfolio.portfolio import Portfolio
 from model.portfolio_manager.portfolio_manager import PortfolioManager
+
+def _calculate_portfolio_var(w, V):
+    # function that calculates portfolio risk
+    w = np.matrix(w)
+    return (w * V * w.T)[0, 0]
+
+def _calculate_risk_contribution(w, V):
+    # function that calculates asset contribution to total risk
+    w = np.matrix(w)
+    sigma = np.sqrt(_calculate_portfolio_var(w, V))
+    # Marginal Risk Contribution
+    MRC = V * w.T
+    # Risk Contribution
+    RC = np.multiply(MRC, w.T) / sigma
+    return RC
+
+def _risk_budget_objective(x, pars):
+    # calculate portfolio risk
+    V = pars[0]  # covariance table
+    x_t = pars[1]  # risk target in percent of portfolio risk
+    sig_p = np.sqrt(_calculate_portfolio_var(x, V))  # portfolio sigma
+    risk_target = np.asmatrix(np.multiply(sig_p, x_t))
+    asset_RC = _calculate_risk_contribution(x, V)
+    return sum(np.square(asset_RC - risk_target.T))  # sum of squared error
+
+def _total_weight_constraint(x):
+    return np.sum(x) - 1.0
+
+def _long_only_constraint(x):
+    return x
+
+
 
 
 class RiskParity(PortfolioManager):
@@ -17,42 +52,61 @@ class RiskParity(PortfolioManager):
         pass
 
     def load_data(self, tickers:list, update=False):
-        d = DataManager().load(ticker=tickers)
+        data = DataManager().load(ticker=tickers)
+        df_ret = pd.DataFrame()
+        for k, v in data.items():
+            tmp = v.market_data
+            df_ret = pd.concat([df_ret, tmp.reset_index()[['date', 'ticker', 'close']]])
+        df_ret = df_ret.pivot(index='date', columns='ticker', values='close')
+        df_ret = np.log(df_ret).diff().fillna(0)
+        df_cov = pd.DataFrame()
+
+        lw = LedoitWolf()
+        for i in range(2, df_ret.shape[0] - 504):
+            window_data = df_ret.iloc[i:i + 504]
+            #exclude assets that have all zeros
+            cov_assets = window_data.columns[~(window_data == 0.0).all(axis=0)]
+            window_data = window_data[cov_assets]
+            lw.fit(window_data.values)
+            if df_cov.empty:
+                df_cov = pd.DataFrame(index=pd.MultiIndex.from_product([[window_data.index[-1]], cov_assets]),
+                                      columns=cov_assets, data=lw.covariance_)
+            else:
+                df_cov = pd.concat([df_cov, pd.DataFrame(index=pd.MultiIndex.from_product([[window_data.index[-1]], cov_assets]),
+                                     columns=cov_assets, data=lw.covariance_)])
+
+        self.data = df_cov
+        pass
 
     def once(self):
         pass
 
     def next(self, date: dt.datetime):
-        current_port = self.portfolio.get_data(date)
+        current_port = self.portfolio.get_data_by_date(date)
         nav = current_port.iat[0, current_port.columns.get_loc('NAV')]
         current_pos = self.portfolio.get_positions_d1(date)
 
-        ticker_buy = None
-        ticker_sell = None
+        #get cov data
+        df_cov = self.data[self.data.index.get_level_values(0) < date]
 
-        if date in self.data.index:
-            i = self.data.index.get_loc(date)
-            if self.data.index[i + 2].month != date.month == 12:
-                # sell current position
-                ticker_sell = current_pos.iloc[0]['ticker']
-                # buy next contract
-                ticker_buy = 'UC' + FutureCode((date.month+1) % 12 + 1).name + str(date.year - 2000 + 1).zfill(2)
+        port_data = self.portfolio.get_data()
+        port_data = port_data[port_data.index < date]
+        #rebal every 1st day of the month
+        if port_data.empty or (df_cov.index.get_level_values(0).max().month != date.month and date in self.data.index.get_level_values(0)):
+            df_cov = self.data[self.data.index.get_level_values(0) == date]
 
-            elif self.data.index[i + 1].month != date.month != 12:
-                # sell current position
-                ticker_sell = current_pos.iloc[0]['ticker']
-                # buy next contract
-                ticker_buy = 'UC' + FutureCode((date.month+1) % 12 + 1).name + str(date.year - 2000 + (1 if (date.month+1) % 12 == 0 else 0)).zfill(2)
-            else:
-                #don't need to do anything unless current position is 0
-                if current_pos.empty:
-                    ticker_buy = 'UC' + FutureCode(date.month+1).name + str(date.year - 2000).zfill(2)
+            selected_assets = df_cov.sum(axis=0)
+            selected_assets = selected_assets.loc[selected_assets != 0.0].index
 
-            if ticker_buy:
-                fut = DataManager().load(ticker_buy)[ticker_buy]
-                self.portfolio.add_order_future(date=date, ticker=ticker_buy, qty=nav / fut.get_close(date=date) / fut.m)
+            df_cov = df_cov[df_cov.index.get_level_values(1).isin(selected_assets)][selected_assets]
+            x = [1 / len(selected_assets)] * len(selected_assets)  # your risk budget percent of total portfolio risk (equal risk)
+            cons = ({'type': 'eq', 'fun': _total_weight_constraint}, {'type': 'ineq', 'fun': _long_only_constraint})
+            bounds = [(0.2, 0.2)] + [(0.0, 0.15) for _ in range(len(selected_assets) - 1)]
+            res = minimize(_risk_budget_objective, x, args=[df_cov.values, x], method='SLSQP', constraints=cons, tol=1e-12, bounds=bounds, options={'disp': False})
+            df_pos = pd.DataFrame(index=[date], columns=selected_assets, data=[res.x])
 
-            if ticker_sell:
-                qty = -current_pos.iloc[0]['qty']
-                self.portfolio.add_order_future(date=date, ticker=ticker_sell, qty=qty)
+            for c in df_pos.columns:
+                if c != 'EWZ US Equity':
+                    self.portfolio.add_order_equity_perc(date=date, ticker=c, perc=df_pos[c].values[0]*0.95)
+
         pass
--- test3.py
+++ test3.py
@@ -2,6 +2,7 @@
 import datetime as dt
 
 from core.portfolio_calculator import PortfolioCalculator
+from model.portfolio.portfolio import Portfolio
 from model.portfolio_manager.risk_parity import RiskParity
 from utils.pcalendar import CalendarType
 
@@ -9,14 +10,15 @@ if __name__ == '__main__':
     print(dt.datetime.now())
 
     name = 'RiskParity'
-
-    pm = RiskParity(name=name, first_trade=dt.datetime(2007, 1, 2),
-                      trade_calendar=CalendarType.B3, portfolio_calendar=CalendarType.BR)
+    p = Portfolio(name=name, begin_date=dt.datetime(2003, 2, 3), currency='USD', calendar=CalendarType.US, cash_index={'BRL':'BZACCETP Index', 'USD':'LD20TRUU Index'})
+    pm = RiskParity(name=name, first_trade=dt.datetime(2003, 2, 3), trade_calendar=CalendarType.US, portfolio=p)
     #update decision data
-    pm.load_data(tickers=['XLF', 'XLB', 'XLC', 'XLE', 'XLI', 'XLK', 'XLP', 'XLRE', 'XLU', 'XLV', 'XLY'])
+    pm.load_data(tickers=['EWZ US Equity', 'XLF US Equity', 'XLB US Equity', 'XLC US Equity',
+                          'XLE US Equity','XLI US Equity', 'XLK US Equity', 'XLP US Equity',
+                          'XLRE US Equity', 'XLU US Equity', 'XLV US Equity', 'XLY US Equity'])
 
     bt = PortfolioCalculator(portfolio_manager=pm)
-    bt.run(end_date=dt.datetime(2025,12,30), run_all=True)
+    bt.run(end_date=dt.datetime(2026,3,31), run_all=True)
 
     print(dt.datetime.now())
     pm.save()
